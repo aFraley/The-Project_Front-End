8a389ea5e0460249038031712e23a22e490cbaf7
aFraley <humanimalmammal@webtv.net>
2017-01-08 11:10:16 -0600

    first commit

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp')
+
+var BASE_DIR = '/home/alan/Code/misc/django-ember/ember-front/dist/'
+
+var paths = {
+    base: BASE_DIR + '*',
+    assets: BASE_DIR + 'assets/*',
+    templatePath: '/home/alan/Code/misc/django-ember/server/frontend/templates/frontend',
+    staticPath: '/home/alan/Code/misc/django-ember/server/frontend/static/frontend'
+};
+
+gulp.task('assets', function(){
+  console.log('copying assets');
+  var stream = gulp.src(
+    '/home/alan/Code/misc/django-ember/ember-front/dist/assets/*'
+  ).pipe(gulp.dest(paths.staticPath));
+  return stream;
+});
+
+
+// Main Task
+gulp.task('default', ['assets'], function(){
+  console.log('building');
+});
---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Running build process..."
+ember build
+gulp
+echo "Finished build process."
